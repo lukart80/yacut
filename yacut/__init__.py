dce37496c99cf23de2ffd64165782f75093396c4
artemmac <lukart80@yandex.ru>
2022-05-06 13:47:56 +0300

    refactoring done

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from .settings import Config
+from settings import Config
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object(Config)
