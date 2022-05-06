755f4939ddfe8b1dd5ca229c13a8bbb0143979ab
artemmac <lukart80@yandex.ru>
2022-05-06 19:00:16 +0300

    added default env

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='YOUR_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
