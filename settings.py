b3477b327b07c9d01f7f41110036f9f681c31989
artemmac <lukart80@yandex.ru>
2022-05-06 13:55:29 +0300

    env added

--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASK_APP = os.getenv('FLASK_APP')
